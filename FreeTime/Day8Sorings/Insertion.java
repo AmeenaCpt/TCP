1c5ad61daf17d1011cc77e2f52dc882fed447d43
AmeenaCpt <shaikashikamina@gmail.com>
2025-09-27 01:06:35 +0530

    Daily update - 2025-09-27

---
+++ FreeTime/Day8Sorings/Insertion.java
@@ -0,0 +1,31 @@
+package FreeTime.Day8Sorings;
+
+import java.util.Scanner;
+
+public class Insertion {
+    public static void solve(int[] a){
+        int n=a.length;
+        for(int i=0;i<=n-1;i++){
+            int j=i;
+            while(j>0 && a[j-1]>a[j]){
+                int temp=a[j-1];
+                a[j-1]=a[j];
+                a[j]=temp;
+                j--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        solve(a);
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+        sc.close();
+    }
+}
---
+++ FreeTime/Day8Sorings/SelectionSort.java
@@ -0,0 +1,30 @@
+package FreeTime.Day8Sorings;
+import java.util.Scanner;
+public class SelectionSort{
+    public static void solve(int[] a,int n){
+        for(int i=0;i<=n-2;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[min]){
+                    min=j;
+                }
+            }
+            int temp=a[min];
+            a[min]=a[i];
+            a[i]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        solve(a, n);
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+        sc.close();
+    }
+}
